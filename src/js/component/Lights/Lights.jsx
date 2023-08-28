406d782b539bb3a0555204e921c57b8645d412f1
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-28 21:14:05 +0000

    useState en funcionamiento

--- src/js/component/Lights/Lights.jsx
+++ src/js/component/Lights/Lights.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css"
 const Lights = () => {
+    const [color, setColor] = useState("red")
     return (
         <div className='container'>
             <div className='sujecion'></div>
             <div className='container-lights'>
-                <div className='light red'></div>
-                <div className='light yellow'></div>
-                <div className='light green'></div>
+                <div onClick={() => setColor("red")} className={`light red +${color === "red" ? " selected" : ""}`}></div>
+                <div onClick={() => setColor("yellow")} className={`light yellow +${color === "yellow" ? " selected" : ""}`}></div>
+                <div onClick={() => setColor("green")} className={`light green +${color === "green" ? " selected" : ""}`}></div>
             </div>
         </div>
     )
 }
-
 export default Lights;
